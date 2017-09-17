c09d7f2d6e4e892246f9d723ab34d5a6f32eb0ab
Leanne Abraham <abraham.leanne@gmail.com>
2017-09-17 15:51:36 -0500

    completed lab tasks 1 & 2

--- Lab1/triangle_area.py
+++ Lab1/triangle_area.py
@@ -1,18 +1,17 @@
 # This program calculates the area of a triangle.
 
 # This is a string that is printed when the program first runs,
-    #it tells you what the program does (ie, calculate the area of a triangle)
+#it tells you what the program does (ie, calculate the area of a triangle)
 print "This program finds the area of a triangle."
-print 	
+print
 
 # This portion of the program creates the two variables that will be used to calculate the area of the triangle
-    #based on numerical user input
-    #
+#based on numerical user input
 height = input("Please enter the height of the triangle: ")
 base = input("Please enter the base length of the triangle: ")
 
-# [explain]
+# This takes the user input of the base and the height of the triangle and finds the area (1/2bh)
 area = 0.5 * height * base
 
-# [explain]
+# this writes the area in area in a readable sentance while reminding the user of their input
 print "The area of a triangle with height", height, "and base", base, "is", area, "."
